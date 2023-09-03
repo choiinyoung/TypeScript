56e64605d9b8e7597b0672c9cd9d187433272afb
choiinyoung <choi121100@naver.com>
2023-09-03 17:55:57 +0900

    TS 환경 재설정
    TS 환경 재설정

--- 0902/dist/Array.js
+++ 0902/dist/Array.js
@@ -5,19 +5,21 @@ console.log(member);
 member.push("ㅎㄷ");
 console.log(member);
 var memberany = ["이뇽", 10, true, null];
-var memberunion = [
-    "이뇽",
-    10,
-    true,
-    null,
-];
+var memberunion = ["이뇽", 10, true, null];
 var str = ["이뇽", 10];
 var str2 = ["ㅎㅎ", 25];
 console.log(str);
 console.log(str2);
-var arr1 = [function () { return "이뇽"; }, function () { return "ㅎㅎ"; }];
+var arr1 = [
+  function () {
+    return "이뇽";
+  },
+  function () {
+    return "ㅎㅎ";
+  },
+];
 console.log(arr1[0]());
 for (var _i = 0, arr1_1 = arr1; _i < arr1_1.length; _i++) {
-    var fn = arr1_1[_i];
-    console.log(fn());
+  var fn = arr1_1[_i];
+  console.log(fn());
 }
