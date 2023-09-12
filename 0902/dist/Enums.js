f7855af2459ad9166c59b54a6f5207648d15842a
choiinyoung <choi121100@naver.com>
2023-09-12 19:19:05 +0900

    Enums
    Enums

---
+++ 0902/dist/Enums.js
@@ -0,0 +1,25 @@
+"use strict";
+var Class;
+(function (Class) {
+    Class[Class["Rock"] = 0] = "Rock";
+    Class[Class["Scissors"] = 1] = "Scissors";
+    Class[Class["Papper"] = 2] = "Papper";
+})(Class || (Class = {}));
+var Class1;
+(function (Class1) {
+    Class1[Class1["Rock"] = 0] = "Rock";
+    Class1[Class1["Scissors"] = 101] = "Scissors";
+    Class1[Class1["Papper"] = 102] = "Papper";
+})(Class1 || (Class1 = {}));
+var Game;
+(function (Game) {
+    Game["Rock"] = "ROCK";
+    Game["Scissors"] = "SCISSORS";
+    Game["Papper"] = "PAPER";
+})(Game || (Game = {}));
+var Game1;
+(function (Game1) {
+    Game1["Rock"] = "ROCK";
+    Game1[Game1["Scissors"] = 2] = "Scissors";
+    Game1[Game1["Papper"] = 3] = "Papper";
+})(Game1 || (Game1 = {}));
