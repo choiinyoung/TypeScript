5d71e02b644241f126e15540bc488d24c95dd5e9
choiinyoung <choi121100@naver.com>
2024-04-17 20:04:34 +0900

    function2
    function2

--- dist/Function.js
+++ dist/Function.js
@@ -27,3 +27,35 @@ function sum3(a, b) {
 }
 console.log(sum3(10, undefined));
 console.log(sum3(10));
+function sum4() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (result, number) { return result + number; }, 0);
+}
+console.log(sum4(10, 20, 30));
+console.log(sum4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+var userf = {
+    name: "inyoung",
+    age: 20,
+    init: function () {
+        var _this = this;
+        return function () {
+            return _this.age;
+        };
+    },
+};
+var getAgef = userf.init();
+var agef = getAgef();
+console.log(agef);
+var Handeler = (function () {
+    function Handeler() {
+    }
+    Handeler.prototype.onClick = function (e) {
+        console.log("clicked!");
+    };
+    return Handeler;
+}());
+var handelr = new Handeler();
+browserEL.addClickListener(handelr.onClick);
